b597c91443a2c0d258430c233decb7a95ff937bf
Musta16 <mostafaaboukelila@gmail.com>
2022-04-24 18:19:31 +0200

    Inserita una funzione per giocare la mossa

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
 console.log("Hello World!");
+
+// Genera un numero random da 1 a 3 e ritorna un valore
+function computerPlay() {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    if (randomNumber === 1) {
+        return "Rock";
+    }
+    else if (randomNumber === 2) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+}
+
+console.log(computerPlay());
