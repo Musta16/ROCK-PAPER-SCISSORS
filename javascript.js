c1db2ec5b26b74212974089390f06373a2022223
Musta16 <mostafaaboukelila@gmail.com>
2022-04-25 08:54:58 +0200

    Inserita la funzione round di gioco e l'input utente

--- javascript.js
+++ javascript.js
@@ -2,16 +2,45 @@ console.log("Hello World!");
 
 // Genera un numero random da 1 a 3 e ritorna un valore
 function computerPlay() {
+
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     if (randomNumber === 1) {
-        return "Rock";
+        return "rock";
     }
     else if (randomNumber === 2) {
-        return "Paper";
+        return "paper";
     }
     else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
-console.log(computerPlay());
+function playerPlay() {
+    
+    let input = prompt("To play insert \'Rock\', \'Paper\' or \'Scissors\'!");
+    let answer = input.toLowerCase();
+    return answer;
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection === computerSelection) {
+        return `It's a tie!`
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return `You lose! ${computerSelection} beats ${playerSelection}.`
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return `You lose! ${computerSelection} beats ${playerSelection}.`
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return `You lose! ${computerSelection} beats ${playerSelection}.`
+    }
+    else return `You Win! ${playerSelection} beats ${computerSelection}.`
+}
+
+const computerSelection = computerPlay();
+const playerSelection  = playerPlay();
+//const playerSelection = "rock"
+console.log(playRound(playerSelection, computerSelection));
+
